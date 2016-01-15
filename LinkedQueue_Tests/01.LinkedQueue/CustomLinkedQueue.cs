60664632bf5e8b012eaf3935bc2bdcf432cc0594
Plamena Miteva <plamenamiteva@abv.bg>
2016-01-15 21:23:46 +0200

    Linked Queue Unit Tests

---
+++ LinkedQueue_Tests/01.LinkedQueue/CustomLinkedQueue.cs
@@ -0,0 +1,100 @@
+﻿namespace _01.LinkedQueue
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class CustomLinkedQueue<T> : IEnumerable<T>
+    {
+        private QueueNode<T> head;
+        private QueueNode<T> tail;
+
+        public int Count { get; private set; }
+
+        public void Enqueue(T element)
+        {
+            var newNode = new QueueNode<T>(element);
+            if (this.Count == 0)
+            {
+                this.head = newNode;
+                this.tail = newNode;
+            }
+            else
+            {
+                this.tail.NextNode = newNode;
+                this.tail = newNode;
+            }
+            this.Count++;
+        }
+
+        public T Dequeue()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("Queue is empty.");
+            }
+
+            var item = this.head.Value;
+            this.head = this.head.NextNode;
+            this.Count--;
+
+            if (this.Count == 0)
+            {
+                this.head = null;
+            }
+
+            return item;
+        }
+
+        public bool Contains(T element)
+        {
+            int index = 0;
+            QueueNode<T> currentNode = this.head;
+            while (currentNode != null)
+            {
+                if (object.Equals(currentNode.Value, element))
+                {
+                    return true;
+                }
+
+                currentNode = currentNode.NextNode;
+                index++;
+            }
+
+            return false;
+        }
+
+        public void Clear()
+        {
+            this.head = null;
+            this.tail = null;
+            this.Count = 0;
+        }
+
+        // Views the first element in the Queue but does not remove it.
+        public T Peek()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("Queue is empty.");
+            }
+
+            return this.head.Value;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var current = this.head;
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.NextNode;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ LinkedQueue_Tests/LinkedQueue.Tests/LinkedQueue.Tests.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _01.LinkedQueue;
+
+namespace LinkedQueue.Tests
+{
+    [TestClass]
+    public class LinkedQueueTests
+    {
+        private static CustomLinkedQueue<int> queue;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            queue = new CustomLinkedQueue<int>();
+        }
+
+        [TestCleanup]
+        public void TestClean()
+        {
+        }
+
+        [TestMethod]
+        public void Count_AddNewElementsInQueue_ShouldReturnCount()
+        {
+            int queueCount = 100;
+            for (int i = 0; i < queueCount; i++)
+            {
+                queue.Enqueue(i);
+            }
+
+            Assert.AreEqual(queueCount, queue.Count, "The queue count is not correct.");
+        }
+
+        [TestMethod]
+        public void Count_AddAndRemoveSeveralNewElementsInQueue_ShouldReturnCorrectCount()
+        {
+            int queueAddCount = 100;
+            for (int i = 0; i < queueAddCount; i++)
+            {
+                queue.Enqueue(i);
+            }
+
+            int queueRemoveCount = 20;
+            for (int i = 0; i < queueRemoveCount; i++)
+            {
+                queue.Dequeue();
+            }
+
+            Assert.AreEqual(queueAddCount - queueRemoveCount, queue.Count, "The queue count is not correct.");
+        }
+
+        [TestMethod]
+        public void Enqueue_NewElementInQueue_ShouldAddElement()
+        {
+            queue.Enqueue(2);
+
+            Assert.AreEqual(1, queue.Count, "The queue count is not correct.");
+        }
+
+        [TestMethod]
+        public void Enqueue_NewElementsInQueue_ShouldAddElementsCorrectly()
+        {
+            var numbersList = new List<int>{2, 3, 4, 5};
+
+            for (int i = 0; i < numbersList.Count; i++)
+            {
+                queue.Enqueue(numbersList[i]);
+            }
+
+            int index = 0;
+            foreach (var element in queue)
+            {
+                Assert.AreEqual(true, element.Equals(numbersList[index]));
+                index++;
+            }
+
+            Assert.AreEqual(4, queue.Count, "The queue count is not correct.");
+        }
+
+        [TestMethod]
+        public void Dequeue_ElementFromQueue_ShouldRemoveFirstElement()
+        {
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+
+            var result = queue.Dequeue();
+
+            Assert.AreEqual(1, queue.Count, "The queue count is not correct.");
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void Dequeue_SeveralElementsFromQueue_ShouldRemoveElementsCorrectly()
+        {
+            var numbersList = new List<int> { 2, 3, 4, 5 };
+
+            for (int i = 0; i < numbersList.Count; i++)
+            {
+                queue.Enqueue(numbersList[i]);
+            }
+
+            int index = 0;
+            foreach (var element in queue)
+            {
+                var removedItem = queue.Dequeue();
+                Assert.AreEqual(removedItem, numbersList[index]);
+                index++;
+            }
+
+            Assert.AreEqual(0, queue.Count, "The queue count is not correct.");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Dequeue_EmptyQueue_ShouldShouldThrow()
+        {
+            queue.Dequeue();
+        }
+        
+    }
+}
